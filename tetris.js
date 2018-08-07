7059e3f81083e52d62efb898e43ae75f325b461b
Tesser4 <kostas@tux.gr>
2018-08-07 08:16:37 +0300

    Create and draw the board

--- tetris.js
+++ tetris.js
@@ -9,9 +9,23 @@ const VACANT = 'white'
 function drawSquare(x, y, color) {
   ctx.fillStyle = color
   ctx.fillRect(y * SQ, x * SQ, SQ, SQ)
-  
+
   ctx.strokeStyle = 'black'
   ctx.strokeRect(y * SQ, x * SQ, SQ, SQ)
 }
 
+let board = []
+for (let r = 0; r < ROW; r++) {
+  board[r] = []
+  for (let c = 0; c < COL; c++) {
+    board[r][c] = VACANT
+  }
+}
+
+function drawBoard() {
+  for (let r = 0; r < ROW; r++) {
+    for (let c = 0; c < COL; c++) {
+      drawSquare(r, c, board[r][c])
+    }
+  }
 }
