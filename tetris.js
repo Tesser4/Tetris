85e34d5f4a7646be0bc7ebf6185db5c6e10fad12
Tesser4 <kostas@tux.gr>
2018-08-07 08:08:51 +0300

    Invert coordinates in drawSquare()

--- tetris.js
+++ tetris.js
@@ -6,11 +6,12 @@ const COL = 10
 const SQ = squareSize = 20
 const VACANT = 'white'
 
-
 function drawSquare(x, y, color) {
   ctx.fillStyle = color
-  ctx.fillRect(x * SQ, y * SQ, SQ, SQ)
+  ctx.fillRect(y * SQ, x * SQ, SQ, SQ)
   
   ctx.strokeStyle = 'black'
-  ctx.strokeRect(x * SQ, y * SQ, SQ, SQ)
+  ctx.strokeRect(y * SQ, x * SQ, SQ, SQ)
+}
+
 }
