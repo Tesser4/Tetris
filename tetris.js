87a304b4811e0dc6c8b15749c63bebb9c06f3466
Tesser4 <kostas@tux.gr>
2018-08-27 11:46:51 +0300

    Introduces gameState and gameParams objs

--- tetris.js
+++ tetris.js
@@ -6,40 +6,60 @@ const scoreElement = document.querySelector('#score')
 const levelElement = document.querySelector('#level')
 const highScoreElement = document.querySelector('#highScore')
 
-const ROW_MAIN = 20
-const COL_MAIN = 10
-const ROW_NEXT = 4
-const COL_NEXT = 4
-const SQ = 30
-const EMPTY = 'white'
+const gameParams = {
+  rowsMain: 20,
+  columnsMain: 10,
+  rowsNext: 4,
+  columnsNext: 4,
+  squareSize: 30,
+  emptySquare: 'white'
+}  
 
-const boardMain = getBoard(ROW_MAIN, COL_MAIN, SQ, EMPTY, ctxMain)
-const boardNext = getBoard(ROW_NEXT, COL_NEXT, SQ, EMPTY, ctxNext)
-let currentBrick = getRandomBrick(boardMain, 0, 3)
-let nextBrick = getRandomBrick(boardNext, 0, 0)
+const gameState = {
+  interval: 500,
+  isOver: false,
+  currentBrick: null,
+  nextBrick: null
+}  
+
+const boardMain = getBoard(
+  gameParams.rowsMain,
+  gameParams.columnsMain,
+  gameParams.squareSize,
+  gameParams.emptySquare,
+  ctxMain
+)
+
+const boardNext = getBoard(
+  gameParams.rowsNext,
+  gameParams.columnsNext,
+  gameParams.squareSize,
+  gameParams.emptySquare,
+  ctxNext
+)
+
+gameState.currentBrick = getRandomBrick(boardMain, 0, 3)
+gameState.nextBrick = getRandomBrick(boardNext, 0, 0)
 
 const score = getScoreManager()
 highScoreElement.innerHTML = score.getHighScore()
 
-let gameInterval = 500
-let gameOver = false
-
 function control(evt) {
   switch (evt.keyCode) {
     case 37:
-      currentBrick.moveLeft()
+      gameState.currentBrick.moveLeft()
       dropStart = Date.now()
       break
     case 38:
-      currentBrick.rotate()
+      gameState.currentBrick.rotate()
       dropStart = Date.now()
       break
     case 39:
-      currentBrick.moveRight()
+      gameState.currentBrick.moveRight()
       dropStart = Date.now()
       break
     case 40:
-      currentBrick.moveDown()
+      gameState.currentBrick.moveDown()
       dropStart = Date.now()
       break
   }
@@ -48,36 +68,36 @@ document.addEventListener('keydown', control)
 
 const boards = [boardMain, boardNext]
 boards.forEach(x => x.draw())
-currentBrick.draw()
-nextBrick.draw()
+gameState.currentBrick.draw()
+gameState.nextBrick.draw()
 
 let dropStart = Date.now()
 ;(function play() {
   let now = Date.now()
   let delta = now - dropStart
 
-  if (delta > gameInterval) {
+  if (delta > gameState.interval) {
     dropStart = Date.now()
-    let locked = currentBrick.moveDown()
+    let locked = gameState.currentBrick.moveDown()
 
     if (locked) {
-      gameOver = currentBrick.lock()
+      gameState.isOver = gameState.currentBrick.lock()
 
-      nextBrick.undraw()
-      currentBrick = nextBrick
-      currentBrick.y = 3
-      currentBrick.board = boardMain
-      currentBrick.draw()
-      nextBrick = getRandomBrick(boardNext, 0, 0)
+      gameState.nextBrick.undraw()
+      gameState.currentBrick = gameState.nextBrick
+      gameState.currentBrick.y = 3
+      gameState.currentBrick.board = boardMain
+      gameState.currentBrick.draw()
+      gameState.nextBrick = getRandomBrick(boardNext, 0, 0)
       
       while (boardMain.hasFullRow()) {
         boardMain.deleteRow(boardMain.hasFullRow())
 
         let levelChanged = score.increasePoints()
         if (levelChanged) {
-          gameInterval = gameInterval === 100
-            ? gameInterval
-            : gameInterval - 100
+          gameState.interval = gameState.interval === 100
+            ? gameState.interval
+            : gameState.interval - 100
           levelElement.innerHTML = score.getLevel()
         }
 
@@ -85,12 +105,12 @@ let dropStart = Date.now()
       }
 
       boards.forEach(x => x.draw())
-      currentBrick.draw()
-      nextBrick.draw()
+      gameState.currentBrick.draw()
+      gameState.nextBrick.draw()
     }
   }
 
-  if (gameOver) {
+  if (gameState.isOver) {
     document.removeEventListener('keydown', control)
 
     if (score.getHighScore() < score.getPoints()) {
